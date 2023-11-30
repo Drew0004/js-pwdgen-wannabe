78a765a89b18217945b6b16d6cc08d4fb5c0b0ef
Drew0004 <drdrewgraphic@gmail.com>
2023-11-30 13:43:16 +0100

    Add prompt user name - surname -color

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Nome
+let userName = prompt('Inserisci il tuo nome:');
+let answerElement1 = document.getElementById('nameAnswer')
+.innerHTML = userName;
+
+//Cognome
+let userSurname = prompt('Inserisci il tuo cognome:');
+let answerElement2 = document.getElementById('surnameAnswer')
+.innerHTML = userSurname;
+
+//Colore preferito
+let userColor= prompt('Qual è il tuo colore preferito?');
+let answerElement3 = document.getElementById('colorAnswer')
+.innerHTML = userColor;
+
+//Somma
+
